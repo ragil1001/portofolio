e78ebe219510db103c50d823effbda64689a8645
ragil1001 <220711672@students.uajy.ac.id>
2025-05-16 00:33:35 +0700

    cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/158Ry16Vnms26IEwxAOBCtQOoHQQGbW-1/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1AGZIgqB495STuW1qlZ5wANvXEAZNPSAo/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
